11cca3fdbe6b6c40c0f6e8a2a2c3f406ab832f27
angular-cli <angular-cli@angular.io>
2016-11-30 10:31:39 +0200

    chore: initial commit from angular-cli

---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'recipe-book-app',
+  templateUrl: 'recipe-book.component.html',
+  styleUrls: ['recipe-book.component.css']
+})
+export class RecipeBookAppComponent {
+  title = 'recipe-book works!';
+}
